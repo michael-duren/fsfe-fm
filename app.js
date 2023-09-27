2565b77c2202310c6e1bca21cef3b6e419002fde
Michael Duren <michaeld@michaelduren.com>
2023-09-27 12:26:01 -0500

    Add dumb html file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const server = require('http').createServer();
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+  res.sendFile('index.html', { root: __dirname }); // root is where we want to serve from
 });
 
 server.on('request', app);
