7bc6163db7a4f250d3af5728549d201aec605c00
Michael Duren <michaeld@michaelduren.com>
2023-09-28 11:04:49 -0500

    update to use in memory db

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.get('/', (req, res) => {
 server.on('request', app);
 server.listen(3000, () => console.log('Listening on 3000'));
 
+process.on('SIGINT', () => {
+  console.log('SIGINT received, shutting down server');
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {});
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+    process.exit(0);
+  });
+});
+
 /** Begin Websockets using ws (npm i ws) */
 const WebSocketServer = require('ws').Server;
 
@@ -27,6 +43,10 @@ wss.on('connection', function connection(ws) {
     ws.send(`Welcome to cyber chat you are user number: ${numClients}`);
   }
 
+  db.run(`INSERT INTO visitors (count, time) 
+      VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
     console.log('A client has disconnected');
@@ -40,3 +60,30 @@ wss.broadcast = function broadcast(data) {
     }
   });
 };
+
+/** End Web Sockets */
+/** Begin database */
+/** Database stuff **/
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+// .seralize ensures DB is set up before any queries
+db.serialize(() => {
+  db.run(`CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+      )`);
+});
+
+function getCounts() {
+  db.each('SELECT * FROM visitors', (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log('shutting down DB');
+  db.close();
+}
